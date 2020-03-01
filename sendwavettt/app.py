c4e27621dd9bea9779b8f9a380e9e2af9a515f98
OmoBe <summersun1978@live.co.uk>
2020-03-01 19:39:21 +0000

    host

--- sendwavettt/app.py
+++ sendwavettt/app.py
@@ -141,12 +141,12 @@ def waveTTTgame():
     return jsonify(boardState)
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    #import os
+    #HOST = os.environ.get('SERVER_HOST', 'localhost')
+    #try:
+        #PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    #except ValueError:
+        #PORT = 5555
+    app.run(debug=True, host='0.0.0.0')#(HOST, PORT)
 
 
